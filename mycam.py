feeedcf9c2ea88763f2ba6f85b56a84cca19b85c
UltralyticsAssistant <web@ultralytics.com>
2026-03-19 02:41:26 +0000

    Auto-format by https://ultralytics.com/actions

--- mycam.py
+++ mycam.py
@@ -3,13 +3,13 @@ import cv2
 from ultralytics import YOLO
 
 model = YOLO(r"yolov8n.pt")
-results=model(
+results = model(
     source=0,
     stream=True,
 )
 
 for result in results:
-    plotted=result.plot()
-    cv2.imshow("yolo inference",plotted)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    plotted = result.plot()
+    cv2.imshow("yolo inference", plotted)
+    if cv2.waitKey(1) & 0xFF == ord("q"):
         break
